b6d4c33501d5b50855c407e0d03ef551589320ed
Martín Álvarez Salazar <sistemas.martin.alvarez@gmail.com>
2024-08-13 22:49:07 -0600

    first

---
+++ NavegacionJerarquica/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavegacionJerarquica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            { 
+              BarBackgroundColor = Color.Crimson
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NavegacionJerarquica/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavegacionJerarquica
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Salta a la vista About
+        async  void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new About());
+        }
+        // Salta a la vista  de cursos
+        async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cursos());
+        }
+    }
+}
